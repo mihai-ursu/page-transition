b5e4dbe22695daa9b9d11087eb65c9585f4c3758
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-11-08 22:24:27 +0200

    Creating files

---
+++ src/pages/recipe-instructions.tsx
@@ -0,0 +1,7 @@
+import styles from "../styles/RecipeInstructions.module.scss";
+
+const RecipeInstructions = () => {
+  return <div>Recipe Instructions</div>;
+};
+
+export default RecipeInstructions;
